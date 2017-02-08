eab0d5dfda18d9ec8e4f468ab0d53a7092119dee
Swapnil Singh <swappp7singh@gmail.com>
2017-02-09 00:25:02 +0530

    connected to database and api is now working!

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+
+var db = mongojs('mongodb://admin:admin@ds145649.mlab.com:45649/mean_todos_app', ['todos']);
+
 
 router.get('/todos', function (req, res, next) {
-    res.send('TODOS API');
+    db.todos.find(function (err, todos) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(todos);
+        }
+    });
 });
 
 module.exports = router;
